770fd2dd9f6da367c05184fedd7e08e6d66df232
123 <allen90423@gmail.com>
2019-10-04 21:12:23 +0800

    123

---
+++ Ch1_Lab2/source/User_defined.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int user_defined_print()
+{
+	printf("welcome to c!\n");
+	return 0;
+
+
+
+
+
+}
---
+++ Ch1_Lab2/source/main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"User_defined.h"
+int main(void)
+{
+
+	user_defined_print();
+
+
+
+
+	system("pause");
+	return 0;
+
+}
